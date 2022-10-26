195d145ea6f78e70af6b1e702db0065da0dfb936
Carl Castillano <carlcastillano2000@gmail.com>
2022-10-26 09:13:27 +0800

    pushing server folder

---
+++ middleware/token/validate-user-token.js
@@ -0,0 +1,21 @@
+const JWT = require('jsonwebtoken')
+const base64decode = require('base-64')
+
+module.exports = async (request, response) => {
+    try {
+        const autHeader = request.headers['user-token']
+        const token = autHeader && autHeader.split(" ")[1]
+        const result = await JWT.verify(token, process.env.privateKEY, (err, res) => {
+            if(err)
+                return response.response('Invalid Token').code(403).takeover()
+            
+            
+            const decodedToken = res.U
+            return base64decode.decode(decodedToken)
+        })
+        
+        return result
+    } catch(err) {
+        return response.response('Something went wrong').code(500).takeover()
+    } 
+}
---
+++ services/announcement.js
@@ -0,0 +1,59 @@
+const { Announcement, User } = require('../models/model-schema');
+
+// const createAnnouncement = async (payload) => {
+//   return await Announcement.create(payload);
+// };
+
+const createAnnouncement = async (id, payload) => {
+  return await Announcement.create(payload, {
+    where: {
+      user_id: id,
+    },
+  });
+};
+
+const deleteAnnouncement = async (id) => {
+  return await Announcement.destroy({
+    where: {
+      id: id,
+    },
+  });
+};
+
+const getAllAnnouncement = async () => {
+  return await Announcement.findAll({
+    attributes: ['id', 'title', 'description', 'content', 'cover_url'],
+    include: [
+      {
+        attributes: ['username'],
+        model: User,
+        as: 'user',
+      },
+    ],
+  });
+};
+
+const getAnnouncementId = async (id) => {
+  return await Announcement.findOne({
+    attributes: ['id'],
+    where: {
+      id: id,
+    },
+  });
+};
+
+const updateAnnouncement = async (id, payload) => {
+  return await Announcement.update(payload, {
+    where: {
+      id: id,
+    },
+  });
+};
+
+module.exports = {
+  createAnnouncement,
+  deleteAnnouncement,
+  getAllAnnouncement,
+  getAnnouncementId,
+  updateAnnouncement,
+};
